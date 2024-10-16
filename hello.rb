550ef6a7d78e4b73b64b7447f0f6cb41821c7a43
laurachaudh <laurachaudhary@chicagobooth.edu>
2024-10-16 22:38:56 +0000

    Oct_16

---
+++ hello.rb
@@ -0,0 +1,14 @@
+pp "howdy, world!"
+
+#require "./bye.rb"
+
+#require "active support/all"
+
+pp "What's your name?"
+
+name = gets.chomp
+
+message = "Good morning, " + name + "!"
+
+pp message
+puts message 
